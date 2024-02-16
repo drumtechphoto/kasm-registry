bb399a22777efaf4491435ed47101ca8b8fe4630
Kevan Ferrara <kevdude10@gmail.com>
2024-02-15 20:00:57 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'drumtechphoto',
+    description: 'The official store for drumtechphoto.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/drumtechphoto/kasm-registry/',
+    contactUrl: 'https://drumtechphoto.com/contact',
   },
   reactStrictMode: true,
   swcMinify: true,
